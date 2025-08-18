68601efe74c61493af163bfdfcfefcd40ce43331
vanalex <apjorquera@gmail.com>
2025-08-19 00:08:38 +0200

    example text cleaning

---
+++ main.py
@@ -0,0 +1,12 @@
+from unstructured.partition.pdf import partition_pdf
+from unstructured.staging.base import elements_to_json
+
+file_path = "files"
+base_file_name = "sample"
+
+def main():
+    elements = partition_pdf(filename=f"{file_path}/{base_file_name}.pdf")
+    elements_to_json(elements=elements, filename=f"{file_path}/{base_file_name}-output.json")
+
+if __name__ == "__main__":
+    main()
